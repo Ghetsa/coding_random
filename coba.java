3270798e0a94e034e496011e9ddeed62f790bf3a
Ghetsa <ghetsa.arryanti@gmail.com>
2023-11-20 13:15:17 +0700

    push all

---
+++ coba.java
@@ -0,0 +1,22 @@
+import java.lang.reflect.Array;
+
+public class coba{
+    public static void main(String[] args) {
+
+        String[][] nilaiAkhir = { {"B+", "C", "B+", "A", "B+"}, {"A", "A", "C+", "B", "B", "C+"}, {"D+", "C"}, {"C+", "B+", "B"} }; 
+        System.out.println(String.join(" ",nilaiAkhir[1]));
+        
+        // for(int i=0; i<=arr.length/2; i++) {
+        //         System.out.println(arr[i]);
+        // }
+        
+        // int hasil; hasil = 0;
+
+        // for(int i=0; i<=1; i++) {
+        //     for(int j=0; j<=2; j++) {
+        //         hasil = j+hasil;
+        //     }
+        // }
+        // System.out.println("Total => "+hasil);
+    }
+}
